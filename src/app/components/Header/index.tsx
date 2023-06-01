469e6c341b37092f9500227d99e1cfd57b38cffb
jheremy <jheremy802@gmail.com>
2023-05-31 20:24:58 -0400

    base catalogo version

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Image from 'next/image'
+import { useState } from 'react';
+
+const Header = ()=>{
+    
+    const [openMenu, setOpenMenu] = useState(false)
+
+    return(
+        <div className='fixed w-screen'>
+        <header className='bg-dark flex justify-between p-4 overflow-hidden rounded-b-3xl items-center'>
+          <div className="">
+            <img src='' alt="App logo"/>
+          </div>
+          <div className="">
+            <button onClick={()=>{
+              setOpenMenu(true)
+            }}>
+              <Image width={120} height={120} src="/vresources/menu.png" alt="" className='w-8' />
+            </button>
+          </div>
+        </header>
+        {
+          openMenu&&
+          <div className="bg-black bg-opacity-70 h-screen absolute top-0 w-screen z-120 text-lowGray">
+            <div className="bg-black p-4">
+              <div className="flex justify-between items-center ">
+                <h3 className='text-3xl font-bold'>
+                  Mas Opciones
+                </h3>
+                <button
+                  onClick={()=>{
+                    setOpenMenu(false)
+                  }}
+                >
+                  <Image width={200} height={200} src="/vresources/closeMenu.png" alt="" className='w-10' />
+                </button>
+              </div>
+              <div className="">
+                <div className="flex flex-col gap-2 p-2 py-5 text-2xl font-semibold">
+                  <div className="">
+                    Ofertas
+                  </div>
+                  <div className="">
+                    Ios
+                  </div>
+                  <div className="">
+                    Samsung
+                  </div>
+                  <div className="">
+                    Otros
+                  </div>
+                  <div className="">
+                    Contactos
+                  </div>
+                </div>
+                <div className="text-center border-b-2 pb-3">
+                  We seek the union between the digital and physical worlds, helping companies 
+                </div>
+              </div>
+            </div>
+  
+          </div>
+        }
+      </div>
+    )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,68 @@
 "use client";
 import Image from 'next/image'
 import { useState } from 'react';
+import Header from './components/Header';
 
 export default function Home() {
 
   const [openMenu, setOpenMenu] = useState(false)
 
-  return (
-    <main className='bg-background'>
-      <header className='bg-dark flex justify-between p-4 overflow-hidden rounded-b-3xl items-center'>
-        <div className="">
-          <img src='' alt="App logo"/>
-        </div>
-        <div className="">
-          <button onClick={()=>{
-            setOpenMenu(true)
-          }}>
-            <Image width={40} height={40} src="/vresources/menu.png" alt="" className='w-8' />
-          </button>
-        </div>
-      </header>
-      {
-        openMenu&&
-        <div className="bg-black bg-opacity-70 h-screen absolute top-0 w-screen z-40 text-lowGray">
-          <div className="bg-black p-4">
-            <div className="flex justify-between items-center ">
-              <h3 className='text-3xl font-bold'>
-                Mas Opciones
-              </h3>
-              <button
-                onClick={()=>{
-                  setOpenMenu(false)
-                }}
-              >
-                <Image width={120} height={120} src="/vresources/closeMenu.png" alt="" className='w-10' />
-              </button>
-            </div>
-            <div className="">
-              <div className="flex flex-col gap-2 p-2 py-5 text-2xl font-semibold">
-                <div className="">
-                  Ofertas
-                </div>
-                <div className="">
-                  Ios
-                </div>
-                <div className="">
-                  Samsung
-                </div>
-                <div className="">
-                  Otros
-                </div>
-                <div className="">
-                  Contactos
-                </div>
-              </div>
-              <div className="text-center border-b-2 pb-3">
-                We seek the union between the digital and physical worlds, helping companies 
-              </div>
-            </div>
-          </div>
+  const devices = [
+    {
+      name: "iphone 14",
+      img: "vresources/phone.png"
+    },
+    {
+      name: "iphone 14",
+      img: "vresources/phone.png"
+    },
+    {
+      name: "iphone 14",
+      img: "vresources/phone.png"
+    },
+    {
+      name: "iphone 14",
+      img: "vresources/phone.png"
+    },
+    {
+      name: "iphone 14",
+      img: "vresources/phone.png"
+    },
+    {
+      name: "iphone 14",
+      img: "vresources/phone.png"
+    },
+  ]
 
+  return (
+    <main className='bg-background py-8'>
+      <div className="flex justify-center flex-col items-center gap-8">
+        <div className="bg-dark w-fit text-white p-2 px-6 text-2xl font-bold shadow-xl rounded-full">
+          Ofertas de la semana
         </div>
-      }
+        <div id='loadMoreB' className="bg-dark w-fit text-white p-2 px-6 text-xl font-medium rounded-full">
+          Ver mas
+        </div>
+        <div className="w-full p-4">
+          <div className="font-bold m-auto bg-dark w-fit text-white p-2 px-6 text-xl rounded-full shadow-lg">
+            Catalogo mas nuevo
+          </div>
+          <div className="grid grid-cols-2 py-6 gap-3">
+            {
+              devices.map((device)=>(
+                <div className=" text-center">
+                  <div className="text-lg mb-2">
+                    {device.name}
+                  </div>
+                  <div className="catItem p-6">
+                    <img src={device.img} alt="" />
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
