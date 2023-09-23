eef93f2e54a84eded5998bfe145e963288ebf6db
jheremois <jheremy802@gmail.com>
2023-09-23 00:06:14 -0400

    add new numbre

--- src/app/components/ProductModal/index.tsx
+++ src/app/components/ProductModal/index.tsx
@@ -65,7 +65,7 @@ export default function ProductModal({open = false, setOpen = (par: any)=>{}, ac
                   <Link 
                     target='_blank'
                     /* href={"https://wa.me/p/6785454001516571/18298395327"} */
-                    href={`https://api.whatsapp.com/send?phone=8298653865&text=${activePhone.name}.`}
+                    href={`https://api.whatsapp.com/send?phone=8293754670&text=${activePhone.name}.`}
                     className="mt-3 inline-flex w-full justify-center rounded-full bg-black text-white font-semibold text-2xl p-2 px-6 "
                   >
                     Comprar
